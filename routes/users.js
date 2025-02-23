8691fdb82ca5c312861f98f91562e9f8de54980d
LudoC <ludo.agtm@outlook.fr>
2025-02-23 16:56:30 +0100

    Add users services

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,16 @@ const service = require('../services/users');
 // Import du middleware pour privatisation
 // PAS ENCORE UTILISER const private = require('../middlewares/private');
 
+// Route pour lister les utilisateurs
+router.get('/', service.getAll);
+// Route pour récupérer les infos d'un utilisateur
+router.get('/:email', service.getByEmail);
 // Route pour créer un utilisateur
 router.put('/add', service.add);
+// La route pour modifier un utilisateur
+router.put('/:email', service.update);
+// La route pour supprimer un utilisateur
+router.delete('/:email', service.delete);
 // Route d'authentification
 router.post('/authenticate', service.authenticate);
 
